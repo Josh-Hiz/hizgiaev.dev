5a5b4c56cc28dba7f5698b5f47b2e4f88c3467ce
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-08-23 19:21:36 -0400

    update global css

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/Components/**/*. {js,ts,jsx,tsx}"],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
