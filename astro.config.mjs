dbe3cbd4964ffc3c6355ebf3a40378144fb3cf01
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-08-23 19:53:33 -0400

    update transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
-    prefetchAll: false
+    prefetchAll: true
   },
   vite: {
     css: {
