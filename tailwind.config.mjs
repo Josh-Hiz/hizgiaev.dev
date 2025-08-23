336eeae1b7cc09a045c99c2c87bc7ae641661538
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-08-23 18:50:06 -0400

    add default tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/Components/**/*. {js,ts,jsx,tsx}"],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
